6067283d29c4c9c42a718048778fa6ff56c58edb
Melissa Castro <melissa.anne.castro@gmail.com>
2019-10-19 18:49:07 -0400

    questions added to survey.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,10 @@
 var friendArray = [
     {
         name: "Kim", 
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj68bODrKnlAhVrg-AKHeZDCKwQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.cosmopolitan.com%2Fuk%2Fentertainment%2Fa29455291%2Fkim-kardashian-defended-prince-harry-meghan-markle%2F&psig=AOvVaw372yWusysa1REVD35h25Qk&ust=1571609565396504",
+        scores: [
+            
+        ]
 
     }
 ]
