be01deba2ee3c92f57234b4e9ae20ed220eef184
Melissa Castro <melissa.anne.castro@gmail.com>
2019-10-19 19:08:41 -0400

    added friends to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,130 @@
 var friendArray = [
     {
         name: "Kim", 
-        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj68bODrKnlAhVrg-AKHeZDCKwQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.cosmopolitan.com%2Fuk%2Fentertainment%2Fa29455291%2Fkim-kardashian-defended-prince-harry-meghan-markle%2F&psig=AOvVaw372yWusysa1REVD35h25Qk&ust=1571609565396504",
+        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/media-personality-kim-kardashian-west-arrives-to-attend-the-news-photo-1570567507.jpg?crop=0.66656xw:1xh;center,top&resize=480:*",
         scores: [
-            
+            4,
+            5,
+            3,
+            2,
+            5,
+            2,
+            5,
+            3,
+            1,
+            4
         ]
-
+    },
+    {
+        name: "Kourtney", 
+        photo: "https://www.hellomagazine.com/imagenes/celebrities/2019101679219/kourtney-kardashian-confuses-fans-new-photo-son-reign/0-382-804/kourtney-kardashian-son-reign-photo-t.jpg",
+        scores: [
+            2,
+            3,
+            3,
+            1,
+            2,
+            3,
+            1,
+            5,
+            4,
+            2
+        ]
+    },
+    {
+        name: "Khloe", 
+        photo: "https://pmcdeadline2.files.wordpress.com/2018/10/khloe-kardashian.jpg?w=681&h=383&crop=1",
+        scores: [
+            3,
+            3,
+            5,
+            5,
+            4,
+            5,
+            2,
+            4,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Kendall", 
+        photo: "https://www.biography.com/.image/t_share/MTQyMDA0OTY4OTA2MzAyNTI2/kendall-jenner_gettyimages-477504950jpg.jpg",
+        scores: [
+            2,
+            1,
+            3,
+            4,
+            4,
+            3,
+            2,
+            1,
+            3,
+            3,
+        ]
+    },
+    {
+        name: "Kylie", 
+        photo: "https://image.insider.com/5da86ed3045a31425b418f23?width=1100&format=jpeg&auto=webp",
+        scores: [
+            4,
+            4,
+            4,
+            1,
+            5,
+            3,
+            1,
+            4,
+            2,
+            4
+        ]
+    },
+    {
+        name: "Kris", 
+        photo: "https://toofab.akamaized.net/2019/08/19/kris-jenner-main-getty-810x610.jpg",
+        scores: [
+            5,
+            1,
+            5,
+            2,
+            3,
+            5,
+            4,
+            5,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Rob", 
+        photo: "https://www.etonline.com/sites/default/files/styles/max_970x546/public/images/2019-01/robk_vidpic.jpg?itok=V27hFImw&h=c673cd1c",
+        scores: [
+            1,
+            4,
+            2,
+            1,
+            1,
+            3,
+            2,
+            2,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Scott", 
+        photo: "https://thenypost.files.wordpress.com/2019/04/scott_disick.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+        scores: [
+            4,
+            3,
+            4,
+            1,
+            5,
+            5,
+            1,
+            4,
+            5,
+            3
+        ]
     }
 ]
